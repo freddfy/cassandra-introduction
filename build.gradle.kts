4991d706f823da15860a905e214bcb8458f48232
Fred Deng <fred.deng.hk@gmail.com>
2021-06-17 22:24:08 +0800

    Update library version

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,11 +13,11 @@ repositories {
 
 dependencies {
     implementation("com.github.nosan:embedded-cassandra:2.0.2")
-    implementation("com.datastax.cassandra:cassandra-driver-core:3.7.1")
-    implementation("com.datastax.cassandra:cassandra-driver-mapping:3.7.1")
+    implementation("com.datastax.cassandra:cassandra-driver-core:3.11.0")
+    implementation("com.datastax.cassandra:cassandra-driver-mapping:3.11.0")
     implementation("org.slf4j:slf4j-simple:1.7.26")
     testImplementation(kotlin("test-junit"))
-    testImplementation("com.github.nosan:embedded-cassandra-test:2.0.2")
+    testImplementation("com.github.nosan:embedded-cassandra-test:2.0.4")
     testImplementation("com.natpryce:hamkrest:1.7.0.0")
 
 }
